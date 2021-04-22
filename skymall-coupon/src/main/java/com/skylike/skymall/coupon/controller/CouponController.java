54e7bcbcc8e1ce0fa2dcd8c937164632aac34c15
stream1080 <fmxhax638@foxmail.com>
2021-04-22 16:14:05 +0800

    nacos配置中心

--- skymall-coupon/src/main/java/com/skylike/skymall/coupon/controller/CouponController.java
+++ skymall-coupon/src/main/java/com/skylike/skymall/coupon/controller/CouponController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,46 @@ import com.skylike.skymall.coupon.service.CouponService;
 import com.skylike.common.utils.PageUtils;
 import com.skylike.common.utils.R;
 
+/**
+ * 1.首先，修改 pom.xml 文件，引入 Nacos Config Starter。
+ *  <dependency>
+ *      <groupId>com.alibaba.cloud</groupId>
+ *      <artifactId>spring-cloud-starter-alibaba-nacos-config</artifactId>
+ *  </dependency>
+ *      2.4以上需要引入一下依赖
+ *  <dependency>
+ *      <groupId>org.springframework.cloud</groupId>
+ *      <artifactId>spring-cloud-starter-bootstrap</artifactId>
+ *      <version>3.0.0</version>
+ *  </dependency>
+ *
+ *  2.在应用的 /src/main/resources/bootstrap.properties 配置文件中配置 Nacos Config 元数据
+ *  spring.application.name=nacos-config-example
+ *  spring.cloud.nacos.config.server-addr=127.0.0.1:8848
+ *
+ *  3.在配置中心添加一个数据集 Data Id skymall-conpon.properties
+ *  4.在controller中添加注解 @Refreshscope 动态获取并刷新配置
+ *    @Value("${配置项变量名}")获取配置。
+ *    如果存在应用配置文件和配置中心的配置文件，则配置中心的为优先配置
+ */
+
+/**
+ * nacos配置中心细节
+ * 1.命名空间，配置隔离
+ *  在boostrap.properties中，配置使用的命名空间的的ID
+ *  每一个微服务之间相互隔离。每一个微服务使用自己的命名空间
+ * 2.配置集：所有的配置的集合
+ * 3.配置ID：类似文件名 Data ID
+ * 4.配置分组
+ *   默认所有的配置集都属于：DEFAULT_GROUP
+ *   可以在boostrap.properties配置其他分组
+ * 5.同时加载多个配置集
+ *   微服务的任何配置信息都可以在配置中心中配置
+ *   只需要在bootstrap.properties中加载配置中心中的配置文件
+ */
+
+
+
 
 
 /**
@@ -24,11 +66,23 @@ import com.skylike.common.utils.R;
  * @email stream@gmail.com
  * @date 2021-04-17 08:36:39
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private Integer age;
+
+    @RequestMapping("/test")
+    public R test(){
+
+        return R.ok().put("name",name).put("age",age);
+    }
 
     /**
      * 模拟一个优惠券
